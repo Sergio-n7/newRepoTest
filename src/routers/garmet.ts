4c1675256b6aa5be85e46820a1b7e128ec62ea25
Sergio Salguero <salgueroflor1982@gmail.com>
2021-06-12 11:59:05 +0200

    updating handlers

--- src/routers/garmet.ts
+++ src/routers/garmet.ts
@@ -2,9 +2,9 @@ import express from 'express'
 
 import {
   createGarmet,
-  findGarmetById,
+  findGarmet,
   deleteGarmet,
-  findAllGarmets,
+  findAll,
   updateGarmet,
   createReview,
 } from '../controllers/garmet'
@@ -16,8 +16,8 @@ import { uploadImage } from '../middlewares/multer.garmet'
 const router = express.Router()
 
 // Every path we define here will get /api/v1/garmets prefix
-router.get('/', findAllGarmets)
-router.get('/:garmetId', findGarmetById)
+router.get('/', findAll)
+router.get('/:garmetId', findGarmet)
 router.put('/:garmetId', isAuthenticated, isAdmin, updateGarmet)
 router.delete('/:garmetId', isAuthenticated, isAdmin, deleteGarmet)
 router.post('/', isAuthenticated, isAdmin, uploadImage, createGarmet)
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,19 +1,19 @@
 import express from 'express'
 
 import {
-  findUserById,
+  findUser,
   createUser,
   deleteUser,
   singInUser,
-  findAllUsers,
+  findAll,
   updateUser,
 } from '../controllers/user'
 
 const router = express.Router()
 
 // Every path we define here will get /api/v1/users prefix
-router.get('/', findAllUsers)
-router.get('/:userId', findUserById)
+router.get('/', findAll)
+router.get('/:userId', findUser)
 router.delete('/:userId', deleteUser)
 router.put('/:userId', updateUser)
 router.post('/', createUser)
